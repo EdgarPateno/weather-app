35e4d6c5238b5eca17d5f12a277399df8935ec45
Edgar Pateño <edgarpateno80@gmail.com>
2023-08-15 20:32:24 +0800

    access weather temperature from the api

--- app.js
+++ app.js
@@ -3,11 +3,32 @@ const https = require('https');
 const app = express();
 
 app.get('/', function(req, res){
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=manila&appid=eab16427d49ab6d0bf55f161eca1f27d'
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=cebu&appid=eab16427d49ab6d0bf55f161eca1f27d'
     https.get(url, function(response){
         console.log(response);
+        console.log(response.statusCode); // status code 200
+        response.on('data', function(data){
+            console.log(data);
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+            // const object = {
+            //     name: 'Edgar',
+            //     anime: 'Flame of Reca',
+            //     skills: 'skill-less'
+            // }
+            // console.log(JSON.stringify(object));
+            const temp = weatherData.main.temp;
+            console.log(temp);
+            // access weather description from the API
+            // access the key value of an array element
+            // access the index of the element which is an object
+            const weatherDescription = weatherData.weather[0].description;
+            console.log(weatherDescription);
+
+            res.send('<h1>temperature' + ' ' + temp + ' degrees Celsius</h1>');
+        })
     });
-    res.send('Weather app version 1.0 Server is up and running');
+    // res.send('Weather app version 1.0 Server is up and running');
 });
 
 app.listen(3000, function(req, res){
